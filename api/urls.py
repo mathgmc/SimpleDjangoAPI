d542e3d0dca6f3efc8d8781fc90178f8af990bc1
Matheus Carvalho <matheusgmc@hotmail.com>
2025-03-12 10:15:37 -0300

    Add Items CRUD

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_data),
+    path('items/', views.get_data, name='get_items'),
+    path('items/', views.post_data, name='create_item'),
+    path('items/<int:pk>/', views.get_data_by_id, name='get_item_by_id'),
+    path('items/<int:pk>/', views.update_data, name='update_item'),
+    path('items/<int:pk>/', views.delete_data, name='delete_item'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,9 +2,50 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Item
 from .serializers import ItemSerializer
+from rest_framework import status
+
 
 @api_view(['GET'])
 def get_data(request):
     items = Item.objects.all()
     serializer = ItemSerializer(items, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def post_data(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_data_by_id(request, pk):
+    try:
+        item = Item.objects.get(pk=pk)
+        serializer = ItemSerializer(item)
+        return Response(serializer.data)
+    except Item.DoesNotExist:
+        return Response({"error": "Item not found"}, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['PUT'])
+def update_data(request, pk):
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        return Response({"error": "Item not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ItemSerializer(item, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_data(request, pk):
+    try:
+        item = Item.objects.get(pk=pk)
+        item.delete()
+        return Response({"message": "Item deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    except Item.DoesNotExist:
+        return Response({"error": "Item not found"}, status=status.HTTP_404_NOT_FOUND)
